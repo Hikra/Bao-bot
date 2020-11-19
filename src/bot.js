8bcf1491b15794aec25f5fc1e54bc3fbb8dc138e
Hikra <hikrapelli@gmail.com>
2020-11-19 16:38:09 +0100

    Update v1.1.0
    + array con suoni disponibili
    - vecchio codice per la riproduzione audio
    + comando 'help' per mostrare i suoni disponibili
    + riprodurre i suoni in modo dinamico

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const discord = require('discord.js');
 const client = new discord.Client();
 
-// const TOKEN = 'NzY2MDIyNzU3NzYyNTk2ODc1.X4dUSQ.56udeNdamMBa9_ziD_zYie0nDz0';
+
 const PREFIX = '!';
 const DelPrefix = '-'; 
 const ban_w = [
@@ -12,16 +12,24 @@ const ban_w = [
                 'negri',
                 'niggas'
                 ];
+const soundboard = [
+                'paguro',
+                'fanculo'
+                ];
 
 
+//#region ----------------- LOGIN --------------
+
+//const TOKEN = 'NzY2MDIyNzU3NzYyNTk2ODc1.X4dUSQ.56udeNdamMBa9_ziD_zYie0nDz0';
 //client.login(TOKEN);
-
+client.login(process.env.token);
 
 client.on('ready', () => {
     console.log(`${client.user.tag} pronto.`);
 });
+//#endregion
 
-client.login(process.env.token);
+
 
 
 const CheckPrefix = (message, cmdName) => message.content.toLowerCase().startsWith(DelPrefix + cmdName);
@@ -29,7 +37,7 @@ const CheckPrefix = (message, cmdName) => message.content.toLowerCase().startsWi
 client.on('message', function(message) {
     if(message.author.bot) return;
 
-    // try {
+    try {
 
         if (CheckPrefix(message, '') && message.channel.id == '570313576301068303') {
             message.delete();
@@ -41,42 +49,34 @@ client.on('message', function(message) {
         else if (ban_w.some(w => message.content.toLowerCase().includes(w))) {
             message.reply('che cazzo fai non si dice');
         }
-        // else if (message.content.toLowerCase() == 'bao') {
-        //     const connection = message.member.voice.channel.join().then(connection => {
-        //         const dispatcher = connection.play(fs.createReadStream('fart.ogg'), { type: 'ogg/opus' });
-
-        //         dispatcher.on('start', () => {
-        //             console.log('audio.mp3 is now playing!');
-        //         });
-    
-        //         dispatcher.on('finish', () => {
-        //             console.log('audio.mp3 has finished playing!');
-        //             connection.disconnect();
-        //         });
-    
-        //         // Always remember to handle errors appropriately!
-        //         dispatcher.on('error', console.error);
-        //     });
-        // }
-        else if (message.content.toLowerCase() == 'bao') {
-            const voiceChannel = message.member.voice.channel;
-            if (!voiceChannel) return message.reply("non sei in un canale vocale.");
-            voiceChannel.join()
-                .then(connection => {
-                    const dispatcher = connection.play('https://www.myinstants.com/media/sounds/movie_1.mp3')
+        else if (message.content.toLowerCase().startsWith('!')) {
+            if (message.content.toLowerCase().includes('help')) {
+                var res = '';
+                soundboard.forEach(x => {
+                    res = `${res}${x}\n`;
+                });
+                message.reply(`Suoni disponibili:\n${res}`);
+            }
+            soundboard.forEach(x => {
+                if (message.content.toLowerCase() == `!${x}`) {
+                    const voiceChannel = message.member.voice.channel;
+                    if (!voiceChannel) return message.reply("non sei in un canale vocale.");
+                    voiceChannel.join()
+                    .then(connection => {
+                        const dispatcher = connection.play(`https://www.mboxdrive.com/${x}.mp3`, {volume: 0.7})
                         .on('finish', () => {
                             voiceChannel.leave();
                         })
                         .on('error', error => {
                             console.log(error);
                         });
-                });
+                    });
+                }
+            });
         }
-        
-
-    // } catch (error) {
-    //     console.log(`ERRORE`);
-    // }
+    } catch (error) {
+        console.log(`ERRORE`);
+    }
     
 });
 
